47dc80e22d2771e3ce181461fe90f8501ecf43f9
protondk <edgarjosue2911@gmail.com>
2022-09-27 21:41:15 -0600

    Tipos de valores

---
+++ valores.js
@@ -0,0 +1,20 @@
+// Valor tipo numero
+40
+
+// Valores string 
+"Edgar Torres"
+
+// Booleans
+true 
+false
+
+// Empty, valores placeholder
+null
+undefined
+
+//Valores tipo objeto
+// Arrays
+[1, 2, 3]
+
+//Objeto
+{name: "Edgar Torres"}
